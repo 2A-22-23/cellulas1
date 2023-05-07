efb922395b8619a340ec83ab4bf68b503ec08f57
NesrineIsaaoui <91319146+NesrineIsaaoui@users.noreply.github.com>
2023-05-07 23:19:38 +0200

    Update sendmail.php
    "update mailing"

--- Admin/journal/sendmail.php
+++ Admin/journal/sendmail.php
@@ -17,13 +17,12 @@ try {
   $mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
   $mail->Port       = 465;                   // set the SMTP port for the GMAIL server
   $mail->isHTML(true);
-  $mail->Username   = "iadh.ferjaoui@esprit.tn";  // GMAIL username
-  $mail->Password   = "211JMT0528";            // GMAIL password
-  $mail->AddReplyTo('iadh.ferjaoui@esprit.tn', 'Ecolayer');
+  $mail->Username   = "nesrine.aissaoui@esprit.tn";  // GMAIL username
+  $mail->Password   = "211JFT1329";            // GMAIL password
+  $mail->AddReplyTo('nesrine.aissaoui@esprit.tn', 'Nesrine');
   $mail->AddAddress($email);
-  $mail->SetFrom('iadh.ferjaoui@esprit.tn', 'Ecolayer');
+  $mail->SetFrom('nesrine.aissaoui@esprit.tn', 'Nesrine');
   $mail->Subject = 'Diagnostique Statut';
-  echo $token;
   $mail->Body = 'Votre Diagnostique est : '.$status; // optional - MsgHTML will create an alternate automatically
   $mail->AltBody ="";
   
@@ -31,9 +30,8 @@ try {
     echo "Message Sent OK</p>\n";
    
     echo "<script type='text/javascript'>";
-echo "alert('Verifiez-votre boite mail!');
-window.location.href='../Connexion.php';";
-echo "</script>";
+    echo "window.location.href='../AfficherDiagnostique.php';";
+    echo "</script>";
 
 
 
